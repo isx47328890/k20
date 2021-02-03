ee110dcb215cba6acad85d221bf112ccd312c5b7
Mark Stanley Santiago Burgos <mssb71@gmail.com>
2021-02-03 09:10:58 +0100

    k20:kserver

--- k20:kserver/install.sh
+++ k20:kserver/install.sh
@@ -8,15 +8,23 @@ cp /opt/docker/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
 
 kdb5_util create -s -P masterkey
 
+
+# Usuaris que s'utilitzaran amb LDAP de IP
 for user in anna pere marta jordi pau user{01..10}
 do
   kadmin.local -q "addprinc -pw k$user $user"
 done
 
 kadmin.local -q "addprinc -pw kmarta marta/admin"
+kadmin.local -q "addprinc -pw kpere pere/admin"
+kadmin.local -q "addprinc -pq kpau pau/admin"
 kadmin.local -q "addprinc -pw ksuper super"
 
-for user in kuser{01..03}
+
+# Usuaris que s'utilitzaran amb /etc/passwd de IP
+for user in kuser{01..06}
 do
   kadmin.local -q "addprinc -pw $user $user"
 done
+
+
